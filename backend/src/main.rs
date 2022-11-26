f9be17c529c8df0866d778efe117d2a7ca3fba5f
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2022-11-26 14:19:18 +0900

    Scaffolding

---
+++ backend/src/main.rs
@@ -0,0 +1,41 @@
+use tonic::{Request, Response, Status};
+use tonic::transport::Server;
+
+pub mod sqttv2 {
+    tonic::include_proto!("sqttv2");
+}
+
+use sqttv2::*;
+use sqttv2::greeter_server::{Greeter, GreeterServer};
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>, // Accept request of type HelloRequest
+    ) -> Result<Response<HelloReply>, Status> { // Return an instance of type HelloReply
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name).into(), // We must use .into_inner() as the fields of gRPC requests and responses are private
+        };
+
+        Ok(Response::new(reply)) // Send back our formatted greeting
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
